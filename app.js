18532f32a66134850263ead48984ba3041a9c570
Jon Ramer <jramer87@gmail.com>
2017-03-07 12:01:33 -0600

    got the loop timing working

--- app.js
+++ app.js
@@ -2,57 +2,117 @@
 
 $(document)
     .ready(() => {
-        let yellow = $('#yellow');
-        let blue = $('#blue');
-        let red = $('#red');
-        let green = $('#green');
-        let colors = [yellow, blue, red, green];
+        let $yellow = $('#yellow');
+        let $blue = $('#blue');
+        let $red = $('#red');
+        let $green = $('#green');
+        let colors = [$yellow, $blue, $red, $green];
         let pattern = [];
         let patternCount = 0;
+        let userArray = [];
+        let userCount = 0;
         let activeGame = false;
+        let power = false;
+        let matching = false;
 
-        $('#start')
+        //Turns the game on/off
+        $('.slider')
             .on('click', () => {
-                activeGame = true;
-                updatePattern(colors);
-                displayPattern(pattern);
+                togglePower();
             });
 
-        //starts the pattern
-        //Grabs a random index from the colors arr and pushes it to the pattern array
-        // Increments patternCount
+        //Initializes a new game
+        $('#start')
+            .on('click', () => {
+                if (power === true) {
+                    initializeGame();
+                }
+            });
+
+        $('.pads')
+            .on('click', (event) => {
+                userArray.push(event.target.id);
+                console.log("userArray:", userArray);
+                console.log("Pattern:", pattern[0][0].id);
+                let patternId = '';
+                let userId = '';
+                if (pattern.length === userArray.length) {
+                    for (var i = 0; i < pattern.length; i++) {
+                        patternId = pattern[i][0].id;
+                        console.log("patternId:", patternId);
+                    }
+                    for (var j = 0; j < userArray.length; j++) {
+                        userId = userArray[j];
+                        console.log("userId:", userId);
+                        console.log("Matching:", patternId === userId);
+
+                    }
+                }
+                if (patternId === userId) {
+                    matching = true;
+                    updatePattern(colors);
+                    displayPattern(pattern);
+                }
+                console.log("Pattern:", pattern);
+            });
+
+
+
+
+
+
+
+
+        //Toggles the power to the game on or off
+        function togglePower() {
+            if (power) {
+                power = false;
+                console.log("Power Off");
+            } else {
+                power = true;
+                console.log("Power On");
+            }
+        }
+
+        function initializeGame() {
+            activeGame = true;
+            patternCount = 0;
+            userCount = 0;
+            pattern = [];
+            userArray = [];
+            updatePattern(colors);
+            displayPattern(pattern);
+            console.log("Game Active");
+        }
+
+        //Updates the pattern with an additional color
         function updatePattern(arr) {
             let randomColor = colors[Math.floor(Math.random() * arr.length)];
             pattern.push(randomColor);
             patternCount += 1;
-            // console.log("Pattern Count:" + patternCount);
         }
 
         //Displays the new pattern.   Toggles the glow class to show the new pattern
         function displayPattern(arr) {
-            for (var i = 0; i < arr.length; i++) {
-                console.log(arr.length);
-                let currentColor = arr[i];
-                let currentColorId = currentColor[0].id;
-                $(currentColor)
-                    .toggleClass(`${currentColorId}Glow`);
+            let i = 0;
+
+            function myLoop() {
                 setTimeout(() => {
+                    console.log(i);
+                    let currentColor = arr[i];
+                    let currentColorId = currentColor[0].id;
                     $(currentColor)
-                        .removeClass(`${currentColorId}Glow`);
-                }, 500);
+                        .toggleClass(`${currentColorId}Glow`);
+                    setTimeout(() => {
+                        $(currentColor)
+                            .removeClass(`${currentColorId}Glow`);
+                    }, 500);
+                    i += 1;
+                    if (i < pattern.length) {
+                        myLoop();
+                    }
+                }, 1000)
             }
-            console.log("loop ends");
+            myLoop();
         }
-
-        function wait(num) {
-            let begin = new Date()
-                .getTime();
-            let stop = begin;
-            while (stop < begin + num) {
-                stop = new Date()
-                    .getTime();
-            }
-        }
-
-
     });
