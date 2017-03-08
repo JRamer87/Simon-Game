68d24f73820e6fd2454c51061f56949244176ba2
Jon Ramer <jramer87@gmail.com>
2017-03-08 17:00:48 -0600

    got the keybindings working

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ $(document)
         let colors = [$yellow, $blue, $red, $green];
         let pattern = [];
         let patternCount = 0;
+        let streak = 0;
         let userArray = [];
         let activeGame = false;
         let power = false;
-        let matching = false;
+        let matching = true;
 
         //Turns the game on/off
         $('.slider')
@@ -25,41 +26,52 @@ $(document)
             .on('click', () => {
                 if (power === true) {
                     initializeGame();
+                    updatePattern(colors);
+                    displayPattern(pattern);
                 }
             });
 
         $('.pads')
             .on('click', (event) => {
-                userArray.push(event.target.id);
-                let patternId = '';
-                let userId = '';
-                for (let i = 0; i < userArray.length; i++) {
-                    if (pattern[i][0].id !== userArray[i]) {
-                        alert("Sorry you missed one.  Try again!")
-                    }
+                if (power === true && activeGame === true) {
+                    updateUserArray(event, userArray);
+                    checkArrays();
+                    updateStreak();
                 }
-                if (pattern.length === userArray.length) {
-                    for (var i = 0; i < pattern.length; i++) {
-                        patternId = pattern[i][0].id;
-                    }
-                    for (var j = 0; j < userArray.length; j++) {
-                        userId = userArray[j];
-                        console.log("Matching:", patternId === userId);
-                    }
-                    if (patternId === userId) {
-                        matching = true;
-                        updatePattern(colors);
-                        displayPattern(pattern);
-                        userArray = [];
-                    } else if (!matching) {
-                        alert('Sorry you missed one!  Try again!');
-
-                    }
-                }
-                console.log("PatternArray:", pattern);
-                console.log("UserArray:", userArray);
             });
 
+
+        // ADD KEYUP EVENTS TO SELECT PADS
+        $(document)
+            .keyup((event) => {
+                if (power === true && activeGame === true) {
+                    if (event.which === 219) {
+                        userArray.push($yellow[0].id);
+                        toggleYellow();
+                        checkArrays();
+                        updateStreak();
+                    } else if (event.which === 221) {
+                        userArray.push($blue[0].id);
+                        toggleBlue();
+                        checkArrays();
+                        updateStreak();
+
+                    } else if (event.which === 13) {
+                        userArray.push($green[0].id);
+                        toggleGreen();
+                        checkArrays();
+                        updateStreak();
+                    } else if (event.which === 222) {
+                        userArray.push($red[0].id);
+                        toggleRed();
+                        checkArrays();
+                        updateStreak();
+                    }
+                }
+
+
+
+            });
 
 
         //Toggles the power to the game on or off
@@ -72,14 +84,12 @@ $(document)
                 console.log("Power On");
             }
         }
-
+        //Configures the initial game state
         function initializeGame() {
             activeGame = true;
             patternCount = 0;
             pattern = [];
             userArray = [];
-            updatePattern(colors);
-            displayPattern(pattern);
             console.log("Game Active");
         }
 
@@ -97,6 +107,7 @@ $(document)
 
             function myLoop() {
                 setTimeout(() => {
+
                     let currentColor = arr[i];
                     let currentColorId = currentColor[0].id;
                     $(currentColor)
@@ -111,6 +122,81 @@ $(document)
                     }
                 }, 1000);
             }
+        }
+
+        //Updates the user array with the color they entered
+        function updateUserArray(event, arr) {
+            arr.push(event.target.id);
+        }
+
+        function checkArrays() {
+            let patternId;
+            let userId;
+            for (let i = 0; i < userArray.length; i++) {
+                if (pattern[i][0].id !== userArray[i]) {
+                    alert("Sorry you missed one.  Try again!")
+                }
+            }
+            if (pattern.length === userArray.length) {
+
+                for (var i = 0; i < pattern.length; i++) {
+                    patternId = pattern[i][0].id;
+                    userId = userArray[i];
+                }
+                if (patternId === userId) {
+                    matching = true;
+                    updatePattern(colors);
+                    displayPattern(pattern);
+                    userArray = [];
+                    streak += 1;
+                } else if (!matching) {
+                    alert('Sorry you missed one!  Try again!');
+                }
+            }
+        }
+
+        function updateStreak() {
+            $('.streak')
+                .text(streak);
+        }
+        //Toggles the glow class
+        function toggleYellow() {
+            $('.yellowPad')
+                .toggleClass('yellowGlow')
+            setTimeout(() => {
+                $('.yellowPad')
+                    .removeClass('yellowGlow');
+            }, 500);
+        }
+
+        //Toggles the glow class
+        function toggleBlue() {
+            $('.bluePad')
+                .toggleClass('blueGlow')
+            setTimeout(() => {
+                $('.bluePad')
+                    .removeClass('blueGlow');
+            }, 500);
+        }
+
+        //Toggles the glow class
+        function toggleRed() {
+            $('.redPad')
+                .toggleClass('redGlow')
+            setTimeout(() => {
+                $('.redPad')
+                    .removeClass('redGlow');
+            }, 500);
+        }
+
+        //Toggles the glow class
+        function toggleGreen() {
+            $('.greenPad')
+                .toggleClass('greenGlow')
+            setTimeout(() => {
+                $('.greenPad')
+                    .removeClass('greenGlow');
+            }, 500);
         }
 
     });
