bb674476015980cd002cb588a68197fee8f562c8
Jon Ramer <jramer87@gmail.com>
2017-10-03 15:18:54 -0500

    Adds server for heroku deployment

---
+++ server.js
@@ -0,0 +1,19 @@
+"use strict";
+
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+  res.render("index.html");
+});
+
+app.listen(PORT, ()=> {
+  console.log(`listening on port ${PORT}`);
+});
+
+module.exports = app;
